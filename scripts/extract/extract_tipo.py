af519e6b8b7891f82eba794c53005f0d1586a95d
Luis Henrique <senseyluiz@gmail.com>
2026-05-19 17:09:11 -0300

    feat(extract/types) Extrai os dados brutos de tiposexistentes de pokemons

---
+++ scripts/extract/extract_tipo.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import os
+
+BASE_URL = "https://pokeapi.co/api/v2/type/"
+OUTPUT_PATH = "../../data/lake/raw/types.json"
+
+def extract_tipo():
+    url = BASE_URL
+    all_types = []
+
+    while url:
+        print(f"Buscando dados de {url}")
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+        all_types.extend(data["results"])
+        url = data["next"] # Busca pela próxima página
+
+    return all_types
+
+def save_to_json(data):
+    os.makedirs(os.path.dirname(OUTPUT_PATH), exist_ok=True)
+    with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    types = extract_tipo()
+    save_to_json(types)
+    print(f"{len(types)} tipos encontrados com sucesso")
+
+
