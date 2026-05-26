72bedcbe3af526e82a8715161a4254b6e5f30b5c
Luis Henrique <senseyluiz@gmail.com>
2026-05-26 14:36:16 -0300

    :sparkles: feat(load/dim_tipo) Insere dados na tabela dim_tipo

--- scripts/load/load_to_dw.py
+++ scripts/load/load_to_dw.py
@@ -1,8 +1,15 @@
 from urllib.parse import quote_plus
+import pandas as pd
+from pathlib import Path
 
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+dim_tipo = pd.read_json(BASE_DIR / "data/processed/dim_tipo.json")
+df_dim_tipo = pd.DataFrame(dim_tipo)
+
 
 # Configurações de conexão
 load_dotenv()
@@ -17,10 +24,11 @@ DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?
 engine = create_engine(DATABASE_URL)
 
 try:
-    with engine.connect() as conn:
-        print("\33[32mConexão bem sucedida!\33[0m")
-        sql = text("SELECT * FROM dim_tipo")
-        result = conn.execute(sql)
-        print(result.fetchall())
+    with engine.begin() as conn:
+        print("\33[32m✔ Conexão bem sucedida!\33[0m")
+        conn.execute(text("DELETE FROM dim_tipo"))
+        df_dim_tipo.to_sql("dim_tipo", con=conn, if_exists='append', index=False)
+        print("\33[32m✔ Dados inseridos com sucesso!\33[0m")
+
 except Exception as e:
-    print(f"\33[31mFalha na conexão: {e}\33[m")
+    print(f"\33[31m✘ Falha na conexão: {e}\33[m")
