6588e538e85ca9dc7d2ba681a3292e2c7cb1e898
Luis Henrique <senseyluiz@gmail.com>
2026-05-26 14:53:31 -0300

    :recycle: Cria a função de load_table para ser reutilizada

--- scripts/load/load_to_dw.py
+++ scripts/load/load_to_dw.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import os
 BASE_DIR = Path(__file__).resolve().parents[2]
 
+# Ler json de dim_tipo
 dim_tipo = pd.read_json(BASE_DIR / "data/processed/dim_tipo.json")
 df_dim_tipo = pd.DataFrame(dim_tipo)
 
@@ -23,12 +24,15 @@ DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?
 
 engine = create_engine(DATABASE_URL)
 
+def load_table(conn, df, table_name):
+    print(f"\n🔄 Carregando tabela: {table_name}")
+    conn.execute(text(f"TRUNCATE TABLE {table_name} CASCADE"))
+    df.to_sql(table_name, con=conn, if_exists='append', index=False)
+    print(f"\33[32m✔ {table_name} carregada com sucesso!\33[0m")
+
 try:
     with engine.begin() as conn:
         print("\33[32m✔ Conexão bem sucedida!\33[0m")
-        conn.execute(text("DELETE FROM dim_tipo"))
-        df_dim_tipo.to_sql("dim_tipo", con=conn, if_exists='append', index=False)
-        print("\33[32m✔ Dados inseridos com sucesso!\33[0m")
-
+        load_table(conn,df_dim_tipo, "dim_tipo")
 except Exception as e:
     print(f"\33[31m✘ Falha na conexão: {e}\33[m")
